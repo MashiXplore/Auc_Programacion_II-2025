d1bf81bc35199fecfbd0f605588db984ca57e758
enriquetarquisinka <enrique.tarqui@jala.university>
2025-11-16 20:13:00 -0400

    Clase de 16 de noviembre

---
+++ Clase_16_11_2025/Ejercicio_4.py
@@ -0,0 +1,12 @@
+import os
+
+nombre = "archivo.txt"
+
+try:
+    os.remove(nombre)
+    print("archivo elimando ")
+except FileNotFoundError:
+    print("error en el archivo")
+    
+print("Ejecutado todo el codigo")    
+
